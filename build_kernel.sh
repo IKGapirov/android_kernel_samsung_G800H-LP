50a31da45ee3d6ecd6adee13463fb9be9bb7c891
IKGapirov <IKGapirov@gmail.com>
2017-12-02 14:41:37 +0500

    defconfig: Add CONFIG_IP_NF_TARGET_TTL

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,6 +5,8 @@ export CROSS_COMPILE=/home/gapirov/Android/utility/arm-eabi-4.7/bin/arm-eabi-
 mkdir output
 
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8228-sec_atlantic3geur_defconfig msm8226-sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+export KBUILD_BUILD_USER=Gapirov
+export KBUILD_BUILD_HOST=Kernel
+make -j2 -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
